646bba543c6bc7d686cd06407bb97d709099c87f
Bharath <bharathparitala18@gmail.com>
2025-01-15 22:21:01 +0530

    Exercise lines in pdf

--- main.py
+++ main.py
@@ -10,24 +10,30 @@ for index,row in df.iterrows():
     pdf.add_page()
 
     #set the Header
-    pdf.set_font(family="Times",style="B",size=20)
+    pdf.set_font(family="Times",style="B",size=24)
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L",ln=1)
-    pdf.line(10,20,200,20)
+
+    for y in range(20,298,10):
+        pdf.line(10,y,200,y)
+
 
     #set footer
     pdf.ln(265)
-    pdf.set_font(family="Times", style="I", size=12)
+    pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(140, 140, 140)
-    pdf.cell(w=0, h=8, txt=row["Topic"], align="R", ln=1)
+    pdf.cell(w=0, h=10, txt=row['Topic'], align="R")
 
-    for i in range(row["Pages"]):
+    for i in range(row["Pages"]-1):
         pdf.add_page()
 
         #set footer
-        pdf.ln(265)
-        pdf.set_font(family="Times", style="I", size=12)
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(140, 140, 140)
-        pdf.cell(w=0, h=8, txt=row["Topic"], align="R", ln=1)
+        pdf.cell(w=0, h=10, txt=row['Topic'], align="R")
+
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
